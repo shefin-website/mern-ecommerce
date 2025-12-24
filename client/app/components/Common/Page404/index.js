32f49458aae8629b51ddf067589f64838affbfa3
Parth Patidar <parth11.patidar@gmail.com>
2025-12-24 19:48:55 +0530

    feat: add navigation button to 404 page

--- client/app/components/Common/Page404/index.js
+++ client/app/components/Common/Page404/index.js
@@ -5,10 +5,17 @@
  */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Button';
 
 const Page404 = () => {
   return (
-    <div className='page-404'>The page you are looking for was not found.</div>
+    <div className='page-404'>
+      <div className='mb-2'>The page you are looking for was not found.</div>
+      <Link to='/'>
+        <Button text='Back to Home' variant='primary' />
+      </Link>
+    </div>
   );
 };
 
