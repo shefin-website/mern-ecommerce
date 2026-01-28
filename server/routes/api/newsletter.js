50dac50de929e871536d4c9827b62a62e86270e2
shefin-website <shefin.website@gmail.com>
2026-01-28 12:22:39 +0530

    Update newsletter.js

--- server/routes/api/newsletter.js
+++ server/routes/api/newsletter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const mailchimp = require('../../services/mailchimp');
-const mailgun = require('../../services/mailgun');
+const mailtrap = require('../../services/mailtrap');
 
 router.post('/subscribe', async (req, res) => {
   const email = req.body.email;
@@ -10,14 +10,6 @@ router.post('/subscribe', async (req, res) => {
   if (!email) {
     return res.status(400).json({ error: 'You must enter an email address.' });
   }
-
-  const result = await mailchimp.subscribeToNewsletter(email);
-
-  if (result.status === 400) {
-    return res.status(400).json({ error: result.title });
-  }
-
-  await mailgun.sendEmail(email, 'newsletter-subscription');
 
   res.status(200).json({
     success: true,
